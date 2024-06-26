b238fc61a05136dc6920e57447762648a43f49f3
jinscoding <92193087+jinscoding@users.noreply.github.com>
2024-06-26 11:30:00 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -2,6 +2,5 @@ const solution = (absolutes, signs) => {
     for (let index = 0; index < signs.length; index++) {
         if (signs[index] === false) absolutes[index] = -absolutes[index];
     }
-    const result = absolutes.reduce((acc, num) => acc + num);
-    return result;
+    return absolutes.reduce((acc, num) => acc + num);
 }
