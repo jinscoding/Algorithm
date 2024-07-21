31a567261e37b1452507107430d417e61080ec89
Hojin Song <hojin9901@naver.com>
2024-07-22 00:07:35 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,10 @@
+const solution = (price) => {
+    let result = 0;
+    
+    if (price >= 500000) result = Math.floor(price - price * 0.2);
+    else if (price >= 300000) result = Math.floor(price - price * 0.1);
+    else if (price >= 100000) result = Math.floor(price - price * 0.05);
+    else result = price
+    
+    return result;
+}
