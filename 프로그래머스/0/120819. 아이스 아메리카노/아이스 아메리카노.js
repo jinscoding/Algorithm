302c98ff7e28f4e56c05666618f79439c1ac0f92
Hojin Song <hojin9901@naver.com>
2024-07-22 00:24:50 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,10 +1,1 @@
-const solution = (money) => {
-    let result = [];
-    const cup = Math.floor(money / 5500)
-    if (money >= 5500){
-        result.push(cup);
-        result.push(money - cup * 5500);
-    } else result = [0, money];
-    
-    return result;
-}
+const solution = (money) => [Math.floor(money / 5500), money % 5500];
